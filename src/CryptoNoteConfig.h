d51af10b2640fdcf8fb6e947093f25cfc55a0077
quanizzgfx <66619588+quanizzgfx@users.noreply.github.com>
2020-07-21 05:54:04 +0200

    ye

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "crazycoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char	   GENESIS_COINBASE_TX_HEX[]					 = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121010d949feec24bb7428d8150737c4e2d9133244c9aa660c8906b040372da769936";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
